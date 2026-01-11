a3d0179cbd52c19876118e86a0b49a8758585c27
Allisson0 <allissonhiaggo@gmail.com>
2026-01-11 09:32:42 -0300

    Criação do menu de usuário

--- src/main/java/br/com/litealura/LiteAluraApplication.java
+++ src/main/java/br/com/litealura/LiteAluraApplication.java
@@ -1,13 +1,21 @@
 package br.com.litealura;
 
+import br.com.litealura.Main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteAluraApplication{
+public class LiteAluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteAluraApplication.class, args);
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        Main main = new Main();
+        main.menu();
+    }
 }
---
+++ src/main/java/br/com/litealura/Main/Main.java
@@ -0,0 +1,71 @@
+package br.com.litealura.Main;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner input = new Scanner(System.in);
+    //Menu padrão
+    private String menu = """
+                =========== Lite - Alura ==========
+                
+                1 - Buscar livro pelo título
+                2 - Listar livros registrados
+                3 - Listar autores registrados
+                4 - Listar autores vivos em um determinado ano
+                5 - Listar livros em um determinado idioma
+                
+                0 - Sair
+                
+                """;
+    private int choose = -1;
+
+    //======= MENU PRINCIPAL =======
+    public void menu(){
+        //Enquanto a escolha for diferente de 0, o programa executará
+        while(choose!=0) {
+            System.out.println(menu);
+            System.out.println("Escolha uma das opções acima: ");
+
+            //Verifica se a opção é um inteiro de fato, caso não, retorna
+            //um aviso.
+            try {
+                choose = input.nextInt();
+                input.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, digite um número.");
+            }
+
+            //Com base na escolha, realiza uma ação.
+            switch (choose){
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                    //Ação de término de comandos
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+
+                    //Opção diferente das indicadas
+                default:
+                    System.out.println("Informe uma opção válida, por favor.");
+            }
+        }
+
+        //Finalização do programa.
+        System.out.println("Programa finalizado com sucesso!");
+    }
+}
