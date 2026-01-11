b56e6520a133ba05def78fb72979f3a56b2f97df
Allisson0 <allissonhiaggo@gmail.com>
2026-01-11 09:19:30 -0300

    Firts commit

---
+++ src/main/java/br/com/litealura/LiteAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.litealura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteAluraApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteAluraApplication.class, args);
+	}
+
+}
